4e0dad782ad992ec357e475044e6eeea4e4baffc
adeel420 <adeelimran467@gmail.com>
2026-01-16 20:59:07 +0500

    api change

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    process.env.NEXT_PUBLIC_API_URL || "https://fyp-backend-blond.vercel.app",
+    process.env.NEXT_PUBLIC_API_URL || "https://fyp-backend-liard-eight.vercel.app",
   withCredentials: true, // Important for cookies/sessions if you use them
   headers: {
     "Content-Type": "application/json",

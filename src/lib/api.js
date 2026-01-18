a79d04b1f1a9743b5f125ced7efea7a556b9d708
Muhammad Kaif <kaiftanveer943@gmail.com>
2026-01-18 22:18:27 +0500

    changes

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    // process.env.NEXT_PUBLIC_API_URL || "https://fyp-backend-liard-eight.vercel.app",
-    "http://localhost:5000",
+    process.env.NEXT_PUBLIC_API_URL || "https://fyp-backend-liard-eight.vercel.app",
+  // "http://localhost:5000",
   withCredentials: true, // Important for cookies/sessions if you use them
   headers: {
     "Content-Type": "application/json",
